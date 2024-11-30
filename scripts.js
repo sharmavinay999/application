1e29d4a6ef1d4324d0293001bbe0645a2a01c1bc
VINAY KUMAR SHARMA <sharmavinaykumar666@gmail.com>
2024-11-30 12:50:39 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,17 @@
 
 document.getElementById('downloadBtn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'path/to/your/file.zip';  // Change to your file's URL
-    link.download = 'yourfile.zip';       // Set the download file name
+    link.href = 'file/vinay.pdf';  // Change to your file's URL
+    link.download = 'vinay1.pdf';       // Set the download file name
+    link.click();  // Trigger the download
+});
+
+
+// Second button
+
+document.getElementById('downloadBtn1').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = 'file/vinay1.pdf';  // Change to your file's URL
+    link.download = 'vinay1.pdf';       // Set the download file name
     link.click();  // Trigger the download
 });
